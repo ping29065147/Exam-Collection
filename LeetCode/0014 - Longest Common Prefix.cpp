457269990e573da63f83fcf0f286e512d337225a
ping29065147 <pinghe.yeh@gmail.com>
2021-10-28 14:39:21 +0800

    20211028 update

---
+++ LeetCode/0014 - Longest Common Prefix.cpp
@@ -0,0 +1,18 @@
+static auto fast_io = []
+{
+    ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+	return 0;
+}();
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs)
+    {
+        sort(strs.begin(), strs.end());
+        int i = 0, l = min(strs[0].size(), strs.back().size());
+        while (i < l && strs[0][i] == strs.back()[i]) ++i;
+        return strs[0].substr(0, i);
+    }
+};
---
+++ LeetCode/0020 - Valid Parentheses.cpp
@@ -0,0 +1,26 @@
+static auto fast_io = []
+{
+    ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+	return 0;
+}();
+
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        if (s.size() % 2) return false;
+        
+        unordered_map<char, char> dict = { {')', '('}, {']', '['}, {'}', '{'} };
+        stack<char> Stk;
+        
+        for (auto& c : s)
+        {
+            if (!Stk.empty() && Stk.top() == dict[c]) Stk.pop();
+            else Stk.push(c);
+        }
+        
+        return Stk.empty();
+    }
+};
